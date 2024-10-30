b3857ad9382ea4bc480c6b51c69eaeb050502b01
Mehebul02 <mehebul2123@gmail.com>
2024-10-31 00:31:34 +0600

    type literal types and optional type added

---
+++ module/dist/2.2.js
@@ -0,0 +1,8 @@
+"use strict";
+// Reference type --->> Object 
+const user = {
+    Name: 'Mehebul Alif',
+    Designation: 'Front End Developer',
+    employerId: 11146,
+    salary: 44,
+};
--- module/src/2.2.ts
+++ module/src/2.2.ts
@@ -1,13 +1,20 @@
 // Reference type --->> Object 
 
 const user: {
+   readonly company:string //Literal type
     Name: string;
     Designation: string;
     employerId: Number;
-    salary?: number
+    salary?: number; //Optional Type
+    isMarried:boolean;
 } = {
+    company:'bdCalling It Ltd',
     Name: 'Mehebul Alif',
     Designation: 'Front End Developer',
     employerId: 11146,
-    salary:
+    salary:44,
+    isMarried:false
 }
+console.log(user);
+
+user.company='Bdcalling'
