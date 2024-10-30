c595273fe2101f3427badf32030b5b5e747d315b
Mehebul02 <mehebul2123@gmail.com>
2024-10-30 23:41:19 +0600

    added the

---
+++ module/dist/1.4.js
@@ -0,0 +1,7 @@
+"use strict";
+// Basic data type
+// string
+const firstName = 'Mehebul Alif'; //Implicit
+console.log(firstName);
+const lastName = 'Ali'; //Explicit
+console.log(lastName);
---
+++ module/src/1.4.ts
@@ -0,0 +1,11 @@
+// Basic data type
+
+// string
+
+const firstName = 'Mehebul Alif' //Implicit
+
+console.log(firstName);
+
+const lastName:string ='Alif' //Explicit
+
+console.log(lastName);
