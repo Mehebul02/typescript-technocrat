db5bf14a3908087069d5f66e306c2b9598394ef0
Mehebul02 <mehebul2123@gmail.com>
2024-10-30 23:47:26 +0600

    added the primitive data type

--- module/src/1.4.ts
+++ module/src/1.4.ts
@@ -1,11 +1,31 @@
 // Basic data type
 
+// Explicit
+
 // string
+const lastName: string = 'Alif'
+
+// Number
+
+let employerId : number=11146;
+
+// boolean
+
+let isAdmin :boolean =true;
+
+// undefined
+
+let x :undefined=undefined;
+
+// null 
+
+let y :null=null
+
+
+console.log(lastName);
+
+
 
 const firstName = 'Mehebul Alif' //Implicit
 
 console.log(firstName);
-
-const lastName:string ='Alif' //Explicit
-
-console.log(lastName);
