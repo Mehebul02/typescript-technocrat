0729abb912f4128694c90bdea782cceda7be17ba
Mehebul02 <mehebul2123@gmail.com>
2024-10-30 23:55:02 +0600

    added the array type

--- module/src/1.4.ts
+++ module/src/1.4.ts
@@ -7,25 +7,24 @@ const lastName: string = 'Alif'
 
 // Number
 
-let employerId : number=11146;
+let employerId: number = 11146;
 
 // boolean
 
-let isAdmin :boolean =true;
+let isAdmin: boolean = true;
 
 // undefined
 
-let x :undefined=undefined;
+let x: undefined = undefined;
 
 // null 
 
-let y :null=null
+let y: null = null
 
 
 console.log(lastName);
 
+// Array 
 
-
-const firstName = 'Mehebul Alif' //Implicit
-
-console.log(firstName);
+let teamMember:string[] = ['Alif','Ashikur','Tufael','Pervej','Riead']
+let roll:number[] =[532509,532462,]
