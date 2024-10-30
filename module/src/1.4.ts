a5155a707719a36886379124bce984f449a752e4
Mehebul02 <mehebul2123@gmail.com>
2024-10-31 00:03:52 +0600

    tuple type added

--- module/src/1.4.ts
+++ module/src/1.4.ts
@@ -27,4 +27,12 @@ console.log(lastName);
 // Array 
 
 let teamMember:string[] = ['Alif','Ashikur','Tufael','Pervej','Riead']
+teamMember.push('Mithan')
 let roll:number[] =[532509,532462,]
+
+
+// tuple ---->> array ------>oder ----->> types of values
+
+let coordinate:[number,number] = [1,5]
+
+let ageName:[number,string] = [21,"Mehebul Alif"]
