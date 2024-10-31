1e6cd5e71299524cd949bdd8398c71a0ae6ad03d
Mehebul02 <mehebul2123@gmail.com>
2024-10-31 21:44:19 +0600

    spered operator

---
+++ module/src/2.4.ts
@@ -0,0 +1,13 @@
+
+
+
+// spread operator
+/// rest operator
+// destructuring operator 
+
+
+const bros1:string[] = ['Alif','Ried','Ashikur'];
+const bros2 :string[]= ['Mehebul','Apo', 'Mithan'];
+
+bros1.push(...bros2)
+
