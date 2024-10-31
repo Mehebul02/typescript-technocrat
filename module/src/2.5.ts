caf861632254a85e4eaf8d54f0e015420797d169
Mehebul02 <mehebul2123@gmail.com>
2024-10-31 22:55:19 +0600

    added destructuring operator

---
+++ module/src/2.5.ts
@@ -0,0 +1,26 @@
+//  destructuring operator 
+{
+    // destructuring object 
+
+    const user = {
+        id: 11146,
+        name: {
+            firstName: 'Mehebul',
+            lastName: 'Alif',
+            designation: 'Front End Developer'
+        },
+        contact: '01642287450',
+        address: 'Dhaka',
+    }
+
+
+    const { contact, name: { lastName } } = user;
+
+    // destructuring array
+
+    const myColig = ['Ashikur', 'Riead', 'Tuafel',"Rhamat",'Mithan','Apo']
+
+    const [,, rahamad,,apu] = myColig
+
+
+}
