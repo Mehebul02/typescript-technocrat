387e1cfc7a8ce163340bd9e71b420bc40205eb0b
Mehebul02 <mehebul2123@gmail.com>
2024-11-03 23:42:44 +0600

    added the guards type

---
+++ module/src/module3/3.3.ts
@@ -0,0 +1,18 @@
+{
+
+    // type guards 
+
+
+    // typeof ---- type guards 
+
+    type Alphanumeric= string | number
+
+
+    const add =(params1: Alphanumeric, params2 :Alphanumeric):Alphanumeric=>{
+        if(typeof params1  === 'number' &&  params2 === 'number'){
+            return params1 + params2
+        }else{
+          return  params1.toString + params2.toString()
+        }
+    }
+}
