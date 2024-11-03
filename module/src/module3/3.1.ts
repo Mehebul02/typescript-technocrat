edd55060ee414e01541303ee4f7dbe21a7638c91
Mehebul02 <mehebul2123@gmail.com>
2024-11-03 18:53:24 +0600

    expolore the class and object

---
+++ module/src/module3/3.1.ts
@@ -0,0 +1,17 @@
+
+{
+
+    // oop class 
+
+    class Animal{
+        name:string;
+        species:string
+        sound:string
+    
+    Constructor(name:string,species:string, sound:string){
+        this.name=name;
+        this.name=species;
+        this.name=sound
+    }
+    }
+}
