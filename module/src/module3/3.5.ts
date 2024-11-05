1d5e40e695dabfe58d65a90a8df4f3cfa010881b
Mehebul02 <mehebul2123@gmail.com>
2024-11-05 20:16:01 +0600

    access modifries

--- module/src/module3/3.5.ts
+++ module/src/module3/3.5.ts
@@ -5,28 +5,56 @@
 {
 
     class BankAccount {
-     public readonly  id: number;
+        public readonly id: number;
         public name: string;
-      private  balance: number;
-
+        protected _balance: number;
 
         constructor(id: number, name: string, balance: number) {
 
             this.id = id;
             this.name = name;
-            this.balance = balance;
-
+            this._balance = balance;
         };
 
-        addDeposit(amount:number){
-            this.balance = this.balance + amount
+        addDeposit(amount: number) {
+            this._balance = this.balance + amount
+        }
+        getBalance() {
+            return this._balance
         }
     };
+    class StudentAccount extends BankAccount {
+        test() {
+            this._balance
+        }
+    }
 
-    const personalAccount = new BankAccount(11146 ,'Mr. Mehebul Alif', 20 );
-    const personalAccount2 = new BankAccount(11147 , 'Mr . Riead Mia', 500);
-    personalAccount.addDeposit(44)
-    personalAccount2.addDeposit(79000)
-    console.log(personalAccount);
-    console.log(personalAccount2);
+    const personalAccount = new BankAccount(11146, 'Mr. Mehebul Alif', 20);
+    const myBalance = personalAccount.getBalance()
+    console.log(myBalance);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    // const personalAccount2 = new BankAccount(11147, 'Mr . Riead Mia', 500);
+    // personalAccount.addDeposit(44)
+    // personalAccount2.addDeposit(79000)
+    // console.log(personalAccount);
+    // console.log(personalAccount2);
 }
